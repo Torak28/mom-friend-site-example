01fdfc57ebabc5c1b78e1de5660c0a67ec50d6a7
Torak28 <zelechowski28@gmail.com>
2019-09-28 15:33:34 +0200

    Looks like Offer looks great! :clap:

--- src/views/Offer.vue
+++ src/views/Offer.vue
@@ -1,5 +1,91 @@
 <template>
   <b-container class="offer">
-    <h1>This is an offer page</h1>
+
+    <b-row class='text'>
+      <b-col>
+        <h1>{{json.title1}}</h1>
+      </b-col>
+    </b-row>
+    <br>
+    <b-row class='text'>
+      <b-col>
+        <b>{{json.sec1}}</b>{{json.text1}}
+      </b-col>
+      <b-col>
+        <b>{{json.sec3}}</b>{{json.text3}}
+      </b-col>
+    </b-row>
+    <br>
+    <b-row class='text'>
+      <b-col>
+          <h3>{{json.subtitle1}}</h3>
+          <br>
+          <br>
+          <b>{{json.sec2}}</b>{{json.text2}}
+          <br>
+          <br>
+          <b>{{json.listTitle}}</b>
+          <br>
+          <ul>
+            <li>{{json.l1}}</li>
+            <li>{{json.l2}}</li>
+            <li>{{json.l3}}</li>
+            <li>{{json.l4}}</li>
+          </ul>
+      </b-col>
+      <b-col>
+        <h3>{{json.subtitle2}}</h3>
+        <br>
+        <br>
+        {{json.text2a}}
+        <b>{{json.text2b}}</b>
+        {{json.text2c}}
+        <b>{{json.text2d}}</b>
+        {{json.text2e}}
+        <br>
+        <br>
+        <b>{{json.listTitle}}</b>
+        <br>
+        <ul>
+          <li>{{json.l5}}</li>
+          <li>{{json.l6}}</li>
+          <li>{{json.l7}}</li>
+          <li>{{json.l8}}</li>
+        </ul>
+      </b-col>
+    </b-row>
+
+    <b-row class='text'>
+      <b-col>
+        <h1>{{json.title2}}</h1>
+      </b-col>
+    </b-row>
+    <br>
+    <b-row class='text'>
+      <b-col>
+        {{json.text4}}
+      </b-col>
+    </b-row>
+
   </b-container>
 </template>
+
+<script>
+import json from '@/json/data.json'
+
+export default {
+  name: "Offer",
+  data() {
+    return {
+      json: json.Offer
+    }
+  }
+};
+</script>
+
+<style scope>
+  .text {
+    text-align: justify;
+    text-justify: inter-word;
+  }
+</style>
