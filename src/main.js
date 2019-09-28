25471a6075c72e9a9c9ef5e0f5c9c748b219fa0c
Torak28 <zelechowski28@gmail.com>
2019-09-28 11:35:10 +0200

    I think icons are installed now?

--- src/main.js
+++ src/main.js
@@ -11,6 +11,18 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import * as VueGoogleMaps from 'vue2-google-maps'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faPhone)
+library.add(faMapMarkerAlt)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.use(BootstrapVue)
 
 Vue.use(VueGoogleMaps, {
